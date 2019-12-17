1dd35722564a55c10fc686ecbba58823a9d757a3
Ke.shidong <keshidong.dev@gmail.com>
2019-12-17 17:45:52 +0800

    Update index.js
    Add makeid to index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { makeid } from './index'
+function makeid (length = 9) {
+  let result = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
 
 const genOneWayChannelId = (channelName, sendPortId, recvPortId) => (
   `${channelName}:${sendPortId}--->${recvPortId}`
